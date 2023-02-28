ef2463324b0d1b6dcee8f88bb616b512c511e3be
Mifkiyan <m.favian199@gmail.com>
2023-03-01 03:52:25 +0700

    feat: bonus 2 done & some minor changes

--- src/ClosestPair.py
+++ src/ClosestPair.py
@@ -4,11 +4,11 @@ import random
 euclideanCalled = 0
 
 
-def randomPoint(n):
+def randomPoint(n, dim):
     arrayPoint = []
-    for i in range(n):
+    for i in range(0, n):
         point = []
-        for j in range(3):
+        for j in range(0, dim):
             sumbu = round(random.uniform(-1000, 1000), 2)
             point.append(sumbu)
         arrayPoint.append(point)
@@ -17,7 +17,7 @@ def randomPoint(n):
 
 def euclideanDistance(point1, point2):
     distance = float(0)
-    for i in range(len(point1)):
+    for i in range(0, len(point1)):
         distance += math.pow((point1[i]-point2[i]), 2)
     global euclideanCalled
     euclideanCalled += 1
@@ -25,14 +25,14 @@ def euclideanDistance(point1, point2):
 
 
 def sortPointbyX(arrayPoint):
-    for i in range(len(arrayPoint)):
+    for i in range(0, len(arrayPoint)):
         for j in range(i, len(arrayPoint)-1):
             if arrayPoint[j][0] > arrayPoint[j+1][0]:
                 arrayPoint[j], arrayPoint[j+1] = arrayPoint[j+1], arrayPoint[j]
     return arrayPoint
 
 
-def closestPairDivideNConquer(arrayPoint, n):
+def closestPairDivideNConquer(arrayPoint, n, dim):
     if (n == 2):
         distance = euclideanDistance(arrayPoint[0], arrayPoint[1])
         result = [distance, arrayPoint[0], arrayPoint[1]]
@@ -41,9 +41,9 @@ def closestPairDivideNConquer(arrayPoint, n):
         d1 = euclideanDistance(arrayPoint[0], arrayPoint[1])
         d2 = euclideanDistance(arrayPoint[0], arrayPoint[2])
         d3 = euclideanDistance(arrayPoint[1], arrayPoint[2])
-        if (min(d1, d2, d3) == d1):
+        if (d1 <= d2) and (d1 <= d3):
             result = [d1, arrayPoint[0], arrayPoint[1]]
-        elif (min(d1, d2, d3) == d2):
+        elif (d2 <= d1) and (d2 <= d3):
             result = [d2, arrayPoint[0], arrayPoint[2]]
         else:
             result = [d3, arrayPoint[1], arrayPoint[2]]
@@ -53,8 +53,8 @@ def closestPairDivideNConquer(arrayPoint, n):
         left = arrayPoint[:mid]
         right = arrayPoint[mid:]
 
-        dleft = closestPairDivideNConquer(left, mid)
-        dright = closestPairDivideNConquer(right, n-mid)
+        dleft = closestPairDivideNConquer(left, mid, dim)
+        dright = closestPairDivideNConquer(right, n-mid, dim)
 
         result = []
 
@@ -70,24 +70,22 @@ def closestPairDivideNConquer(arrayPoint, n):
         else:
             midPoint = arrayPoint[mid][0]
 
-        for i in range(n):
-            if (arrayPoint[i][0] >= midPoint - result[0]) and (arrayPoint[i][0] <= midPoint + result[0]):
+        for i in range(0, n):
+            if (arrayPoint[i][0] >= (midPoint - result[0])) and (arrayPoint[i][0] <= (midPoint + result[0])):
                 strip.append(arrayPoint[i])
 
         if (len(strip) > 1):
-            a = 0
-            b = 1
-            distance = euclideanDistance(strip[a], strip[b])
-            for i in range(len(strip)):
+            for i in range(0, len(strip)):
                 for j in range(i+1, len(strip)):
-                    temp = euclideanDistance(strip[i], strip[j])
-                    if (temp < distance):
-                        distance = temp
-                        a = i
-                        b = j
-
-            if (distance < result[0]):
-                result = [distance, strip[a], strip[b]]
+                    found = True
+                    for k in range(dim):
+                        if found:
+                            found = found and (
+                                abs(strip[i][k]-strip[j][k]) < result[0])
+                    if (found):
+                        distance = euclideanDistance(strip[i], strip[j])
+                        if (distance < result[0]):
+                            result = [distance, strip[i], strip[j]]
 
         return result
 
@@ -96,7 +94,7 @@ def closestPairBruteForce(arrayPoint, n):
     distance = euclideanDistance(arrayPoint[0], arrayPoint[1])
     result = [distance, arrayPoint[0], arrayPoint[1]]
 
-    for i in range(n):
+    for i in range(0, n):
         for j in range(i+1, n):
             temp = euclideanDistance(arrayPoint[i], arrayPoint[j])
             if (temp < distance):
--- src/main.py
+++ src/main.py
@@ -2,17 +2,23 @@ import ClosestPair as util
 import time
 import platform
 
+dim = int(input("Masukkan banyak dimensi : "))
+
+while dim < 2:
+    print("Masukan tidak valid. Dibutuhkan minimal 2 dimensi\n")
+    dim = int(input("Masukkan banyak dimensi : "))
+
 n = int(input("Masukkan banyak titik : "))
 
 while n < 2:
     print("Masukan tidak valid. Dibutuhkan minimal 2 titik untuk menghitung jarak\n")
     n = int(input("Masukkan banyak titik : "))
 
-arrayPoint = util.randomPoint(n)
+arrayPoint = util.randomPoint(n, dim)
 arrayPoint = util.sortPointbyX(arrayPoint)
 
 startDnC = time.time()
-resultDnC = util.closestPairDivideNConquer(arrayPoint, n)
+resultDnC = util.closestPairDivideNConquer(arrayPoint, n, dim)
 timeDnC = time.time() - startDnC
 euclideanDnC = util.euclideanCalled
 
@@ -22,19 +28,19 @@ timeBF = time.time() - startBF
 euclideanBF = util.euclideanCalled - euclideanDnC
 
 
-print("\n========= DIVIDE AND CONQUER =========")
-print("Point 1:", resultDnC[1])
-print("Point 2:", resultDnC[2])
-print("Distance between them:", resultDnC[0])
-print("Execution time:", timeDnC, "s")
+print("\n=================== DIVIDE AND CONQUER ===================")
+print("Point 1                :", resultDnC[1])
+print("Point 2                :", resultDnC[2])
+print("Distance between them  :", resultDnC[0])
+print("Execution time         :", timeDnC, "s")
 print("Euclidean function is called", str(euclideanDnC) + "x\n")
 
 
-print("========= BRUTE FORCE =========")
-print("Point 1:", resultBF[1])
-print("Point 2:", resultBF[2])
-print("Distance between them:", resultBF[0])
-print("Execution time:", timeBF, "s")
+print("====================== BRUTE FORCE =======================")
+print("Point 1                :", resultBF[1])
+print("Point 2                :", resultBF[2])
+print("Distance between them  :", resultBF[0])
+print("Execution time         :", timeBF, "s")
 print("Euclidean function is called", str(euclideanBF) + "x\n")
 
 print("Run at", platform.platform(), platform.processor(), platform.machine())
